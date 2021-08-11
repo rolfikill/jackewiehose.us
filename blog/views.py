6bb3a773e081b4f4879dba645a17098c9f07bfed
rolfikill <88607693+rolfikill@users.noreply.github.com>
2021-08-11 20:53:28 +0200

    Zusammenspiel View und Modals

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
